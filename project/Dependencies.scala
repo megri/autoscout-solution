a0bd690509e30b6ed9d6e1a73185f25efb7b7ac3
megri <teknocide@gmail.com>
2017-08-30 00:39:47 +0200

    more dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,8 +7,10 @@ object Dependencies {
     Seq(
       "io.circe" %% "circe-core" % version,
       "io.circe" %% "circe-generic" % version,
+      "io.circe" %% "circe-generic-extras" % version,
       "io.circe" %% "circe-parser" % version,
-      "io.circe" %% "circe-literal" % version
+      "io.circe" %% "circe-literal" % version,
+      "io.circe" %% "circe-java8" % version
     )
   }
 
@@ -18,8 +20,21 @@ object Dependencies {
     Seq(
       "org.http4s" %% "http4s-dsl" % version,
       "org.http4s" %% "http4s-blaze-server" % version,
-      "org.http4s" %% "http4s-blaze-client" % version
+      "org.http4s" %% "http4s-blaze-client" % version,
+      "org.http4s" %% "http4s-circe" % version
     )
+  }
+
+  def logback = {
+    val version = "1.2.3"
+
+    Seq("ch.qos.logback" % "logback-classic" % version)
+  }
+
+  def log4s = {
+    val version = "1.3.5"
+
+    Seq("org.log4s" %% "log4s" % version)
   }
 
   def scalaTest = {
@@ -28,6 +43,6 @@ object Dependencies {
     Seq("org.scalatest" %% "scalatest" % version % Test)
   }
 
-  def all = circe ++ http4s ++ scalaTest
+  def all = circe ++ http4s ++ logback ++ log4s ++ scalaTest
 }
 
